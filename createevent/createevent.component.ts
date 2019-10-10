5e4880b0fd14fca6ffcb40226c21eddfd319ba97
thedunerats <thedunerats@yahoo.com>
2019-10-09 19:22:40 -0700

    Current iteration of event creation
    Has map and form fields. Still need to populate the info window of the marker with the data from form fields.

---
+++ createevent/createevent.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { htmlAstToRender3Ast } from '@angular/compiler/src/render3/r3_template_transform';
+import { HttpClient } from '@angular/common/http'; // keep these here for now.
+
+@Component({
+  selector: 'app-createevent',
+  templateUrl: './createevent.component.html',
+  styleUrls: ['./createevent.component.css']
+})
+export class CreateeventComponent implements OnInit {
+
+  constructor() { }
+
+
+
+
+
+  lat = 32.7299; //these get dynamically updated every time the user clicks the page.
+  lng = -97.1140;
+
+  centerlat = this.lat; //center of the map. never changes.
+  centerlng = this.lng;
+
+  htmlStr: any;
+
+  latStr: any; //lat and long for output purposes, may remove later
+  longStr: any;
+
+
+  readonly ROOT_URL = ''
+
+
+  data: string[] = []; 
+
+  ngOnInit() {
+
+  }
+
+  map = document.getElementById('map'); //initializes map on the back end, used event binding on front to do so.
+
+  getLatAndLong(event){ // gets latitude and longitude from a map click
+    this.latStr = event.coords.lat;
+    this.longStr = event.coords.lng;
+    //update the marker with the clicked coordinates
+    this.lat = event.coords.lat;
+    this.lng = event.coords.lng;
+  }
+   
+
+    // this.es.getAllTags();
+
+
+
+  }
